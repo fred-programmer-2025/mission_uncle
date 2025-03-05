ff24c25783f2be23ebccedbc3031b8c44ad77c04
Troy <likemusicinformation@gmail.com>
2025-03-05 20:32:15 +0800

    jason-v1

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,18 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import UncleInfoPage from "../pages/UncleInfoPage";
 import UncleInfoDetailPage from "../pages/UncleInfoDetailPage";
-import Articles from "../pages/Articles";
+import ArticlePage from "../pages/ArticlePage";
 import Admin from "../pages/Admin";
 import CartPage from "../pages/CartPage";
 import CartFormPage from "../pages/CartFormPage";
-import OrderPage from "../pages/OrderPage"
+import OrderPage from "../pages/OrderPage";
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/mission" element={<UncleInfoPage />} />
       <Route path="/mission/:id" element={<UncleInfoDetailPage />} />
-      <Route path="/articles" element={<Articles />} />
+      <Route path="/articles" element={<ArticlePage />} />
       <Route path="/admin" element={<Admin />} />
       <Route path="/cart" element={<CartPage />} />
       <Route path="/cartForm" element={<CartFormPage />} />
