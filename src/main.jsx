c886021468026fd862b1a96c4e5a65af67e79446
eddieyclee <chuan1072@gmail.com>
2025-03-15 23:30:36 +0800

    '再改回BrowserRouter'

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './styles/components/main.scss'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter basename="/mission_uncle">
+  <BrowserRouter basename="/mission_uncle">
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
 )
