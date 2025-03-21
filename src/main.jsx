b539018171240bb38602647e00eabf31422de05c
cbs <wasdjk7634@gmail.com>
2025-03-21 21:25:49 +0800

    修正分頁404，表單重新輸入的問題，Order頁面修正

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './styles/components/main.scss'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/mission_uncle">
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 )
